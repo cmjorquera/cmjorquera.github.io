aa7e8e651241c5947df95155d6c3c301746b124a
Cristian Jorquera <cm.jorquerag@gmail.com>
2024-09-06 11:12:48 -0400

    probando PHP

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php  $saludo ="hola mindo"?>
+<body>
+    <h1>Cristian Jorquera</h1>
+    <p> <?php echo $saludo; ?></p>
+
+</body>
+</html>
